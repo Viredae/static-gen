28c1a5e41d4da063bd3ed3fd31a86c80b8c28412
Viredae <Viredae@gmail.com>
2025-05-31 12:53:47 +0300

    one more try

--- src/main.py
+++ src/main.py
@@ -11,7 +11,7 @@ dir_path_content = "./content"
 template_path = "./template.html"
 
 
-def main(basepath = "/"):
+def main(basepath = "/static-gen/"):
     print("Deleting public directory...")
     if os.path.exists(dir_path_public):
         shutil.rmtree(dir_path_public)
